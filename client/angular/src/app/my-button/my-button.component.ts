2e3f3d36a354cee42e9fbe1c8d64f13f42c92f88
Hemerson Vianna <hemerson.lourenco@gmail.com>
2017-04-23 15:45:30 -0300

    chore(style): adjustments of layout [#18]

--- client/angular/src/app/my-button/my-button.component.ts
+++ client/angular/src/app/my-button/my-button.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-my-button',
+  selector: 'button[app-my-button], a[app-my-button]',
+  host: {
+    '(click)': 'onClick()',
+    '[class.default]': 'buttonType === "default"',
+    '[class.primary]': 'buttonType === "primary"',
+    '[class.secundary]': 'buttonType === "secundary"',
+    '[class.nav]': 'buttonType === "nav"',
+    '[class.small]': 'size === "small"',
+    '[class.medium]': 'size === "medium"',
+    '[class.large]': 'size === "large"',
+    '[class.button--withIcon]': 'icon',
+    '[class.button--withIcon-left]': 'iconPosition === "left"',
+    '[class.button--withIcon-right]': 'iconPosition === "right"'
+  },
   templateUrl: './my-button.component.html',
   styleUrls: ['./my-button.component.scss']
 })
 export class MyButtonComponent implements OnInit {
 
-  @Input() type: string = 'default';
+  @Input() buttonType: string = 'default';
   @Input() size: string = 'medium';
   @Input() icon: any = false;
+  @Input() iconPosition: string = 'left';
 
   constructor() { }
 
--- client/angular/src/app/search-input/search-input.component.ts
+++ client/angular/src/app/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search-input',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-input.component.scss']
 })
 export class SearchInputComponent implements OnInit {
+
+  @Input() perTitle: boolean = false;
 
   constructor() { }
 
